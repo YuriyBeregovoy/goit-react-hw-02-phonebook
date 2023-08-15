3bc27971e6ad24c604582083a5cd8b829512a4aa
YuriyBeregovoy <rik0681@gmail.com>
2023-08-15 11:02:49 +0300

    added the App initial markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react"
+import { nanoid } from 'nanoid'
 
 export class App extends Component {
 
@@ -9,13 +10,23 @@ state = {
 
 
   render() {
-    return (
+    return (<div>
+      <h1>Phonebook</h1>
+      <div>
+        <h3>Name</h3>
         <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-/>
+      type="text"
+      name="name"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+        />
+        <button type="button">Add contact</button>
+      </div>
+        <h2>Contacts</h2>
+        <ul>
+          <li></li>
+        </ul>
+</div>
   );}
 };
