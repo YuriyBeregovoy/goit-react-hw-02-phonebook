1d8293cd56d5ec982dbb436ff6d23127059b1668
YuriyBeregovoy <rik0681@gmail.com>
2023-08-16 22:30:40 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,10 +4,6 @@ export const ContactList = ({contacts, onContactDelete}) => {
   return <ul>
     {contacts.map(contact => (
       <ContactListItem contact={contact} onContactDelete={onContactDelete} />
-          // <li key={contact.id}>
-          //   <p>{contact.name}: {contact.number}</p>
-          //   <button type="button" onClick={() => onContactDelete(contact.id)}>Delete</button>
-          // </li> 
           ))}
         </ul>
 }
