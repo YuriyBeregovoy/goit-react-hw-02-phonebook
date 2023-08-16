ae70e87a51a138df38acee2ae75904416a892a0a
YuriyBeregovoy <rik0681@gmail.com>
2023-08-16 23:04:40 +0300

    Update ContactListItem.styled..js

--- src/components/ContactListItem/ContactListItem.styled..js
+++ src/components/ContactListItem/ContactListItem.styled..js
@@ -13,12 +13,11 @@ export const ItemButton = styled.button`
     color: inherit;
     border: 1px solid gray;
     border-radius: 4px;
-    background-color: #f4f4fd;
-
-    &:hover, :focus 
-    {
-    color: #4d5ae5;
     background-color: white;
+
+    &:hover, &:focus 
+    {
+    color: #dbcd0b;
     box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 2px 1px rgba(0, 0, 0, 0.08),
     0px 2px 2px rgba(0, 0, 0, 0.12);}
 
