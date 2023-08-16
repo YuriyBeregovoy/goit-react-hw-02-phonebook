61b43876bc36ed4301136fe2c55d7d6d95dc1da3
YuriyBeregovoy <rik0681@gmail.com>
2023-08-16 22:44:45 +0300

    added styles of ContactList component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,10 @@
 import { ContactListItem } from "components/ContactListItem/ContactListItem"
+import { List } from "./ContactList.styled"
 
 export const ContactList = ({contacts, onContactDelete}) => {
-  return <ul>
+  return <List>
     {contacts.map(contact => (
       <ContactListItem contact={contact} onContactDelete={onContactDelete} />
           ))}
-        </ul>
+        </List>
 }
