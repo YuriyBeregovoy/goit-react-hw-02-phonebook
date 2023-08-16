3ab15dd083917ea0481912fd14be463a82960354
YuriyBeregovoy <rik0681@gmail.com>
2023-08-16 23:21:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyle';
-import { Layout } from 'components/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout>
+    
       <App />
       <GlobalStyle/>
-    </Layout>
+   
   </React.StrictMode>
 );
