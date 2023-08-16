f7fc903febb4d76e6d121d2959843b3c35f85d21
YuriyBeregovoy <rik0681@gmail.com>
2023-08-16 22:44:49 +0300

    Update ContactForm.styled.js

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -17,12 +17,27 @@ margin-bottom: 10px;
 
 export const FormButton = styled.button`
 
-display: block;
+    display: block;
     max-width: 169px;
     align-self: center;
+    padding: 5px 10px;
     font-family: inherit;
     font-weight: 400;
     font-size: 14px;
-    line-height: 1.5;
+    line-height: 1.2;
+    text-align: center;
     letter-spacing: 0.04em;
+    color: inherit;
+    border: 1px solid gray;
+    border-radius: 4px;
+    background-color: #f4f4fd;
+
+    &:hover, :focus 
+    {
+    color: #4d5ae5;
+    background-color: white;
+    box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 2px 1px rgba(0, 0, 0, 0.08),
+    0px 2px 2px rgba(0, 0, 0, 0.12);}
+
+
 `;
